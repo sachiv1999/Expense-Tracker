8093ef17ff7218fcc77cc33ca7ebc195fb6dcb63
sachiv1999 <sachiv.chapadiya131951@marwadiuniversity.ac.in>
2025-11-03 22:30:25 +0530

    Update MainActivity.java

--- JAVA/MainActivity.java
+++ JAVA/MainActivity.java
@@ -4,12 +4,12 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
 import android.database.Cursor;
 
-
 import android.view.View;
-import android.widget.TableLayout;
-import android.widget.TableRow;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -18,12 +18,10 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     EditText etExpenseName, etAmount, etCategory, etDate;
-    Button btnAddExpense,btnHistory, btnGraph ;
-    TableLayout tableLayout;
+    Button btnAddExpense ;
+    
     DBHelper dbHelper;
-//    ArrayList<String> expenseList;
-//    ArrayAdapter<String> adapter;
-//    private View view;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,28 +32,18 @@ public class MainActivity extends AppCompatActivity {
         etCategory = findViewById(R.id.etCategory);
         etDate = findViewById(R.id.etDate);
         btnAddExpense = findViewById(R.id.btnAddExpense);
-        tableLayout = findViewById(R.id.tableLayout);
-        btnHistory = findViewById(R.id.btnHistory);
-        btnGraph = findViewById(R.id.graph);
+
+        // Load ExampleFragment
+        Expense_bottom fragment = new Expense_bottom();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        // Begin and commit transaction
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.fragment_container, fragment);
+        transaction.commit();
 
         dbHelper = new DBHelper(this);
-        loadExpenses();
-        
-        btnGraph.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, ExpenseGraph.class);
-                startActivity(intent);
-            }
-        });
-
-        btnHistory.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, ExpenseList.class);
-                startActivity(intent);
-            }
-        });
+       
 
         btnAddExpense.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -79,7 +67,7 @@ public class MainActivity extends AppCompatActivity {
                     etAmount.setText("");
                     etCategory.setText("");
                     etDate.setText("");
-                    loadExpenses();
+                    
                 } else {
                     Toast.makeText(MainActivity.this, "Failed to add expense", Toast.LENGTH_SHORT).show();
                 }
@@ -89,39 +77,5 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-    private void loadExpenses() {
-        // Remove old rows except header
-        int childCount = tableLayout.getChildCount();
-        if (childCount > 1) {
-            tableLayout.removeViews(1, childCount - 1);
-        }
-
-        Cursor cursor = dbHelper.getAllExpenses();
-        if (cursor.moveToFirst()) {
-            do {
-                TableRow row = new TableRow(this);
-
-                TextView tvName = new TextView(this);
-                tvName.setText(cursor.getString(1));
-
-                TextView tvAmount = new TextView(this);
-                tvAmount.setText(String.valueOf(cursor.getDouble(2)));
-
-                TextView tvCategory = new TextView(this);
-                tvCategory.setText(cursor.getString(3));
-
-                TextView tvDate = new TextView(this);
-                tvDate.setText(cursor.getString(4));
-
-                row.addView(tvName);
-                row.addView(tvAmount);
-                row.addView(tvCategory);
-                row.addView(tvDate);
-
-                tableLayout.addView(row);
-            } while (cursor.moveToNext());
-        }
-        cursor.close();
-    }
 
 }
